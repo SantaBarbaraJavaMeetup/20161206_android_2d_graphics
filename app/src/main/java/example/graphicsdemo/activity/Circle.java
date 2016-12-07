a5b08c93abe296e21ee3e0356907bb299b092960
Elliot Barlas <elliotbarlas@gmail.com>
2016-12-06 16:39:37 -0800

    rename ball to circle.

--- app/src/main/java/example/graphicsdemo/activity/Circle.java
+++ app/src/main/java/example/graphicsdemo/activity/Circle.java
@@ -8,10 +8,12 @@ import android.view.animation.Interpolator;
 
 import java.util.Random;
 
-public class Ball {
+public class Circle {
 
     private static final Interpolator interpolator = new BounceInterpolator();
+
     private static final Random random = new Random();
+
     private static final int FRAMES = 80;
     private static final int DIAMETER = 50;
 
@@ -20,20 +22,18 @@ public class Ball {
     private float left;
     private int frame;
 
-    public Ball(int canvasWidth) {
-        this(random.nextFloat() * (canvasWidth - DIAMETER),
-                random.nextInt(FRAMES),
-                Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+    public Circle(int canvasWidth) {
+        this(random.nextFloat() * (canvasWidth - DIAMETER), random.nextInt(FRAMES));
     }
 
-    public Ball(float left, int frame, int color) {
+    public Circle(float left, int frame) {
         paint = new Paint();
-        paint.setColor(color);
+        paint.setColor(Color.WHITE);
 
         this.left = left;
         this.frame = frame;
     }
-    
+
     public void draw(Canvas canvas) {
         float fraction = interpolator.getInterpolation((float) frame / FRAMES);
 
--- app/src/main/java/example/graphicsdemo/activity/DemoAnimation.java
+++ app/src/main/java/example/graphicsdemo/activity/DemoAnimation.java
@@ -6,23 +6,23 @@ import example.graphicsdemo.animation.Animation;
 
 public class DemoAnimation implements Animation {
 
-    private static final int COUNT = 10;
+    private static final int COUNT = 50;
 
-    private Ball[] array;
+    private Circle[] circles;
 
     @Override
     public void draw(Canvas canvas) {
-        if (array == null) {
-            array = new Ball[COUNT];
-            for (int i = 0; i< array.length; i++) {
-                array[i] = new Ball(canvas.getWidth());
+        if (circles == null) {
+            circles = new Circle[COUNT];
+            for (int i = 0; i< circles.length; i++) {
+                circles[i] = new Circle(canvas.getWidth());
             }
         }
 
         canvas.drawARGB(255, 0, 0, 0);
 
-        for (Ball ball : array) {
-            ball.draw(canvas);
+        for (Circle circle : circles) {
+            circle.draw(canvas);
         }
     }
 
